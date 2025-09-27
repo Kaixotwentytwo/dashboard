91e9a06a69b3c9995edf8b3fbefa6c8c163c83a6
Kaixotwentytwo <ilasav761@gmail.com>
2025-09-27 12:51:03 +0300

    no

--- visual.js
+++ visual.js
@@ -8,7 +8,7 @@ let amountOfColumns = getFirstStrokeFromCSV("").length;
 let isExceptionHere = false;
 let exceptColumn;
 
-console.log("Yes");
+console.log("No");
 
 //function createSelectElements(amountOfColumns) {
 //	let count = amountOfColumns;
