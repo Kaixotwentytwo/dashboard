53c4d87c75963babac542ec6f31efd44e84362d6
Kaixotwentytwo <ilasav761@gmail.com>
2025-09-27 12:55:54 +0300

    we can work with it

--- processing.js
+++ processing.js
@@ -4,7 +4,7 @@ let pickColumnElements = document.querySelectorAll(".pickColumn");
 let firstBothEls = new Set([pickColumnElements[0], pickColumnElements[1]]);
 let exceptions = new Set([]);
 
-console.log("ohuet")
+console.log("Working version, isn't it?");
 
 // Выбираются колонки для выбора на основе исключений
 function pickOptions(setOfSelects, arrayOfColumns, setOfExceptions) {
